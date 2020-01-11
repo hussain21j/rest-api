b865e3d6031c1669bb9c68c87208be2fdc94a29e
Tayab Hussain <hussain21j@gmail.com>
2020-01-11 15:50:34 +0100

    base rest api created

---
+++ src/test/java/com/project/bdd/CucumberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
